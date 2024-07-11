a40457f35b323c6bc9ce274c71eeb317254a02fe
EasyDae <98295040+EasyDae@users.noreply.github.com>
2024-07-11 10:42:56 +0900

    Create 02023.cpp

---
+++ 02XXX/02023.cpp
@@ -0,0 +1,41 @@
+https://www.acmicpc.net/problem/2023
+
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+bool isprime(int num)
+{
+    for (int i = 2; i * i <= num; ++i)
+        if (num % i == 0)
+            return 0;
+    return 1;
+}
+
+void DFS(int num, int len)
+{
+    if (len == n)
+    {
+        cout << num << '\n';
+        return 0;
+    }
+
+    for (int i = 1; i < 10; ++i)
+        if (isprime(num * 10 + i))
+            DFS(num * 10 + i, len + 1);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    DFS(2, 1);
+    DFS(3, 1);
+    DFS(5, 1);
+    DFS(7, 1);
+}
