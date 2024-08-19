b98838cebcb62d119fced0f31fb9b747eed6d477
EasyDae <98295040+EasyDae@users.noreply.github.com>
2024-08-19 21:27:35 +0900

    Create 04406.cpp

---
+++ SWEA/D3/04406.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t; cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        string s; cin >> s;
+        string tmp = "";
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                continue;
+            tmp += s[i];
+        }
+
+        cout << '#' << i << ' ' << tmp << '\n';
+    }
+}
