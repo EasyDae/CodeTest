8973cf22b4f67036ea39a6aa0c59b03229c3df3c
EasyDae <98295040+EasyDae@users.noreply.github.com>
2024-08-21 21:32:20 +0900

    Create 12368.cpp

---
+++ SWEA/D3/12368.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t; cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        int a, b; cin >> a >> b;
+        cout << '#' << i << ' ' << (a + b) % 24 << '\n';
+    }
+}
