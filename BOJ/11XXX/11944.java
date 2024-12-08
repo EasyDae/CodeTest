a6a4cf94d808a64804b5081a4f074040257e362f
EasyDae <98295040+EasyDae@users.noreply.github.com>
2024-12-09 01:06:23 +0900

    Update 11944.java

--- BOJ/11XXX/11944.java
+++ BOJ/11XXX/11944.java
@@ -10,16 +10,16 @@ public class Main{
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         String n = st.nextToken();
-		    int N = Integer.valueOf(n);
-		    int m = Integer.valueOf(st.nextToken());
-		    int len = n.length();
+	int N = Integer.valueOf(n);
+	int m = Integer.valueOf(st.nextToken());
+	int len = n.length();
 		
-		    for(int i = 0; i < N * len && i <= m; i += len)
-			    sb.append(n);
+	for(int i = 0; i < N * len && i <= m; i += len)
+		sb.append(n);
 
-		    if(sb.length() > m)
-			    System.out.print(sb.toString().substring(0, m));
-		    else
-			    System.out.print(sb.toString());
+	if(sb.length() > m)
+		System.out.print(sb.toString().substring(0, m));
+	else
+		System.out.print(sb.toString());
     }
 }
