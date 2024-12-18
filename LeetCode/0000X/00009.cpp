baf0aa609b25ca4546c4a00b119ad4f239f7e9c5
EasyDae <98295040+EasyDae@users.noreply.github.com>
2024-12-18 22:35:41 +0900

    Create 00009.cpp

---
+++ LeetCode/0000X/00009.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/palindrome-number/description/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s1 = to_string(x);
+        string s2 = s1;
+        reverse(s1.begin(), s1.end());
+        return s2 == s1 ? true : false;
+    }
+};
