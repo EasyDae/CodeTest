dc79eb3be3cdd8e9ec5b39d8fdf0944c6f7ecbb7
EasyDae <98295040+EasyDae@users.noreply.github.com>
2024-12-22 22:36:41 +0900

    Create 00014.cpp

---
+++ LeetCode/000XX/00014.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/longest-common-prefix/description/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = strs[0];
+        for(int i=1; i<strs.size(); ++i){
+            string tmp;
+            for(int j=0; j<ans.size(); ++j){
+                if(j==0 && strs[i][0] != ans[0])
+                    return "";
+                if(strs[i][j] != ans[j])
+                    break;
+                tmp += strs[i][j];
+            }
+            ans = tmp;
+        }
+        return ans;
+    }
+};
