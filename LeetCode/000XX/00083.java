ec1685ac212ca899eb72f79d3759da3537c35692
EasyDae <98295040+EasyDae@users.noreply.github.com>
2024-12-24 22:12:27 +0900

    Create 00083.java

---
+++ LeetCode/000XX/00083.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        boolean[] seen = new boolean[201];
+
+        ListNode list = new ListNode(0);
+        ListNode cur = list;
+        
+        while (head != null) {
+            int val = head.val + 100;
+            if (!seen[val]) {
+                seen[val] = true;
+                cur.next = new ListNode(head.val);
+                cur = cur.next;
+            }
+            head = head.next;
+        }
+        
+        return list.next;
+    }
+}
