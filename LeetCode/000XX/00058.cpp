5c45f285f3c641b0bca6e2ad99a486ae8f1dea43
EasyDae <98295040+EasyDae@users.noreply.github.com>
2024-12-24 21:53:20 +0900

    Create 00058.cpp

---
+++ LeetCode/000XX/00058.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/length-of-last-word/description/
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        for(int i=s.size()-1; i>=0; --i){
+            if(s[i] != ' '){
+                while(i >= 0 && s[i] != ' '){
+                    i--;
+                    ans++;
+                }
+                break;
+            }
+        }
+        return ans;
+    }
+};
