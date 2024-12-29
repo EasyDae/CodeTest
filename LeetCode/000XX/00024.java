b2ee7b7f17f8d6480a0d5d4e91cdae979091ee65
EasyDae <98295040+EasyDae@users.noreply.github.com>
2024-12-29 22:00:07 +0900

    Create 00024.java

---
+++ LeetCode/000XX/00024.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/swap-nodes-in-pairs/
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        
+        ListNode tmp = head.next;
+        
+        head.next = swapPairs(tmp.next);
+        tmp.next = head;
+        
+        return tmp;
+    }
+}
