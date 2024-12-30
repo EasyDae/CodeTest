6e119ac902606c97d84a83140e06eefd6336f8d6
EasyDae <98295040+EasyDae@users.noreply.github.com>
2024-12-30 23:21:55 +0900

    Create 00020.java

---
+++ LeetCode/000XX/00020.java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); ++i){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[')
+                stack.push(ch);
+            else{
+                if(stack.size() == 0)
+                    return false;
+                if(ch == ')'){
+                    if(stack.peek() == '(')
+                        stack.pop();
+                    else return false;
+                } else if(ch == '}'){
+                    if(stack.peek() == '{')
+                        stack.pop();
+                    else return false;
+                } else if(ch == ']'){
+                    if(stack.peek() == '[')
+                        stack.pop();
+                    else return false;
+                }
+            }
+        }
+        if(stack.size() != 0)
+            return false;
+        return true;
+    }
+}
