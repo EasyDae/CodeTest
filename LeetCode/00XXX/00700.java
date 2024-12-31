ca1c3e978a6f50c69399f4ea54d2d2f3c5a57c1c
EasyDae <98295040+EasyDae@users.noreply.github.com>
2024-12-31 22:59:28 +0900

    Create 00700.java

---
+++ LeetCode/00XXX/00700.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null || root.val == val)
+            return root;
+        
+        if(val < root.val)
+            return searchBST(root.left, val);
+        return searchBST(root.right, val);
+    }
+}
